baeab25ba45d554c0dff65f9f165361d6ad211c1
Piter Velasquez <pealveco@gmail.com>
2026-04-20 14:27:57 -0500

    add methods for lead management in LeadRepository interface

--- domain/model/src/main/java/co/com/onemillion/model/lead/gateways/LeadRepository.java
+++ domain/model/src/main/java/co/com/onemillion/model/lead/gateways/LeadRepository.java
@@ -1,4 +1,34 @@
 package co.com.onemillion.model.lead.gateways;
 
+import co.com.onemillion.model.lead.Lead;
+import co.com.onemillion.model.lead.LeadSource;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
 public interface LeadRepository {
+    Lead save(Lead lead);
+
+    Optional<Lead> findById(Long id);
+
+    Optional<Lead> findByEmail(String email);
+
+    List<Lead> findAll(int page, int limit, LeadSource fuente, LocalDateTime createdAtFrom, LocalDateTime createdAtTo);
+
+    Lead update(Lead lead);
+
+    boolean softDelete(Long id);
+
+    boolean existsByEmail(String email);
+
+    long countAll();
+
+    Map<LeadSource, Long> countByFuente();
+
+    BigDecimal averagePresupuesto();
+
+    long countLast7Days();
 }
